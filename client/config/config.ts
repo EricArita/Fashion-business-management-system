c973f8524b8131adb8680ff3ce2a9275c090957e
Bui Phan Tho <buiphantho6199@gmail.com>
2020-07-13 22:04:19 +0700

    update code base for front-end

---
+++ client/config/config.ts
@@ -0,0 +1,14 @@
+export const config = {
+    appName: 'fashion-management-system',
+    // categoryId: ['thly8A3mH', '-LPhYCwvO', '5SZOh8nJo', 'zrLKr0Be-'],
+    firebase: {
+    },
+    url: {
+      apiServer: 'http://localhost:3001/',
+    },
+    cookies: {
+      domain: 'localhost',
+      maxAge: 60 * 60 * 1000,
+    },
+  };
+  
